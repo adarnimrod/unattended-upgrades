3c26d3ba9a90ae8675441a61e145a2e247a87353
Adar Nimrod <nimrod@shore.co.il>
2016-12-14 00:50:58 +0200

    - Fix test when using Docker.

--- tests/test_unattended_upgrades.py
+++ tests/test_unattended_upgrades.py
@@ -1,3 +1,8 @@
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+
+
 def test_unattended_upgrades(Service, Package, File, Command):
     assert Package('unattended-upgrades').is_installed
     assert Service('unattended-upgrades').is_enabled
