accc77abdefe58f2d83f854864c837303a38b987
Adar Nimrod <nimrod@shore.co.il>
2016-12-14 01:09:14 +0200

    - Seperated tests.

--- tests/test_unattended_upgrades.py
+++ tests/test_unattended_upgrades.py
@@ -3,8 +3,19 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_unattended_upgrades(Service, Package, File, Command):
-    assert Package('unattended-upgrades').is_installed
+def test_unattended_upgrades_service(Service):
     assert Service('unattended-upgrades').is_enabled
+
+
+def test_unattended_upgrades_package(Package):
+    assert Package('unattended-upgrades').is_installed
+
+
+def test_unattended_upgrades_config(File):
     assert File('/etc/apt/apt.conf.d/50unattended-upgrades').is_file
-    assert Command('/usr/bin/unattended-upgrade --dry-run').rc == 0
+    assert File('/etc/apt/apt.conf.d/02periodic').is_file
+
+
+def test_unattended_upgrades_dry_run(Command, Sudo):
+    with Sudo():
+        assert Command('/usr/bin/unattended-upgrade --dry-run').rc == 0
