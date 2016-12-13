c253afa4d4dc7a2a6c86e1e122377b6f3dc3b0ad
Adar Nimrod <nimrod@shore.co.il>
2016-12-13 23:45:01 +0200

    - Corrected a test.

--- tests/test_unattended_upgrades.py
+++ tests/test_unattended_upgrades.py
@@ -2,4 +2,4 @@ def test_unattended_upgrades(Service, Package, File, Command):
     assert Package('unattended-upgrades').is_installed
     assert Service('unattended-upgrades').is_enabled
     assert File('/etc/apt/apt.conf.d/50unattended-upgrades').is_file
-    assert Command('/usr/bin/unattended-upgrade --dry-run')
+    assert Command('/usr/bin/unattended-upgrade --dry-run').rc == 0
